6978a5ec182a7f82d924832af2b5fafdcc466bcc
Daniel-Sean Folkes <65097622+DSeanHD@users.noreply.github.com>
2024-05-03 21:48:11 -0500

    Uploaded the necessary files

---
+++ script.js
@@ -0,0 +1,29 @@
+let userInput = document.getElementById("user-input");
+let checkBtn = document.getElementById("check-btn");
+let clearBtn = document.getElementById("clear-btn");
+let resultsDiv = document.getElementById("results-div");
+
+function checkNumber() {
+  const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+
+  if (userInput.value == "") {
+    return alert("Please provide a phone number");
+  }
+
+  let item = document.createElement("li");
+  resultsDiv.appendChild(item);
+
+  if (regex.test(userInput.value)) {
+    item.innerHTML = "Valid US number: " + userInput.value;
+    userInput.value = "";
+  } else {
+    item.innerHTML = "Invalid US number: " + userInput.value;
+    userInput.value = "";
+  }
+}
+
+checkBtn.addEventListener("click", checkNumber);
+
+clearBtn.addEventListener("click", () => {
+  resultsDiv.innerHTML = "";
+})
